b3fb8228ce06b6b98713ebe0a16f588cfae56526
jorge <jorge.oyaneder.velasquez@gmail.com>
2020-10-18 19:36:46 -0300

    Para un hilo. (Respaldo)

--- src/main/java/tarea1/Tarea.java
+++ src/main/java/tarea1/Tarea.java
@@ -1,36 +1,21 @@
 package tarea1;
 
+import org.apache.commons.lang3.time.StopWatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * The Primes Problem.
+ */
 public class Tarea {
+
     /**
-     * Function to test primality.
-     *
-     * @param n the number to test.
-     * @return true if n is prime
+     * The Logger.
      */
-    public static boolean isPrime(final long n) {
+    private static final Logger log = LoggerFactory.getLogger(Tarea.class);
 
-        // Can't process negative numbers
-        if (n <= 0) {
-            throw new IllegalArgumentException("Error in n: Can't process negative numbers");
-        }
-
-        // "One" isn't prime!
-        if (n == 1) {
-            return false;
-        }
-
-        // Testing primality from 2 to n-1
-        // TODO: Change to n/2 the upper limit (??)
-        for (long i = 2; i < n / 2; i++) {
-
-            // If module == 0 -> not prime!
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
-
-    }
 
     /**
      * The main.
@@ -39,96 +24,94 @@ public class Tarea {
      */
     public static void main(String[] args) {
 
-        // Number to evaluate
-        long number = 100000;
+        // The Chrono
+        final StopWatch stopWatch = StopWatch.createStarted();
 
-        //The counter of primes
-        long contador = 0;
+        log.debug("Stating the Main ...");
 
-        // Two Threads
-        Thread thread_1_for_2 = new Thread();
-        Thread thread_2_for_2 = new Thread();
+        /**
+         * For One Thread
+         *
+         * Thread thread = new Thread(new PrimeTask(82731237L));
+         *
+         * // log.debug("Prime? {}.", PrimeTask.isPrime(82731237L));
+         *
+         * thread.run();
+         */
 
-        // Conditions for 2 Threads
-        thread_1_for_2.condition(1, number / 2);
-        thread_2_for_2.condition(number / 2, number);
 
+        // Time:
+        log.info("Done in {}.", stopWatch);
+    }
 
-        // Three Threads
-        Thread thread_1_for_3 = new Thread();
-        Thread thread_2_for_3 = new Thread();
-        Thread thread_3_for_3 = new Thread();
+    /**
+     * Class to calculate the prime.
+     */
+    private static class PrimeTask implements Runnable {
 
-        // Conditions for 3 Threads
-        thread_1_for_3.condition(1, number / 3);
-        thread_2_for_3.condition(number / 3, 2 * (number / 3));
-        thread_3_for_3.condition(2 * (number / 3), number);
+        /**
+         * The Number
+         */
+        private static long number;
 
-        // Four Threads
-        Thread thread_1_for_4 = new Thread();
-        Thread thread_2_for_4 = new Thread();
-        Thread thread_3_for_4 = new Thread();
-        Thread thread_4_for_4 = new Thread();
+        /**
+         * The Counter.
+         */
+        private final AtomicInteger counter = new AtomicInteger(0);
 
-        // Conditions for 4 Threads
-        thread_1_for_4.condition(1, number / 4);
-        thread_2_for_4.condition(number / 4, number / 2);
-        thread_3_for_4.condition(number / 2, 3 * (number / 4));
-        thread_4_for_4.condition(3 * (number / 4), number);
+        /**
+         * The Constructor.
+         *
+         * @param number to test.
+         */
+        public PrimeTask(final long number){
+            this.number = number;
+        }
 
-        // The time now
-        long start = System.currentTimeMillis();
-
-        /*
-        // One Thread
-        for(long i = 1; i < number; i++){
-            if(isPrime(i)){
-                // System.out.println(i + " is Prime");
-                contador++;
+        /**
+         * Run the code. (Parallel Mode)
+         */
+        @Override
+        public void run() {
+            if (isPrime(this.number)){
+                log.debug("{} was a Prime !!!", this.number);
+                counter.getAndIncrement();
+            }
+            else{
+                log.debug("{} was not a Prime !!!", this.number);
             }
         }
-        */
 
-        /*
-        // Two Threads
-        thread_1_for_2.run();
-        thread_2_for_2.run();
+        /**
+         * Function to test primality.
+         *
+         * @param n the number to test.
+         * @return true is n is prime
+         */
+        private static boolean isPrime(final long n) {
 
-        // Total prime numbers of two threads
-        contador += thread_1_for_2.num_Primes();
-        contador += thread_2_for_2.num_Primes();
-        */
+            // Can't process negative numbers
+            if (n <= 0) {
+                throw new IllegalArgumentException("Error in n: Can't process negative numbers");
+            }
 
-        /*
-        // Three Threads
-        thread_1_for_3.run();
-        thread_2_for_3.run();
-        thread_3_for_3.run();
+            // "One" isn't prime!
+            if (n == 1) {
+                return false;
+            }
 
-        // Total prime numbers of three threads
-        contador += thread_1_for_3.num_Primes();
-        contador += thread_2_for_3.num_Primes();
-        contador += thread_3_for_3.num_Primes();
-        */
+            // Testing primality from 2 to n-1
+            // TODO: Change to n/2 the upper limit (??)
+            for (long i = 2; i < n / 2; i++) {
 
-        /*
-        // Four Threads
-        thread_1_for_4.run();
-        thread_2_for_4.run();
-        thread_3_for_4.run();
-        thread_4_for_4.run();
+                // If module == 0 -> not prime!
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
 
-        // Total prime numbers of three threads
-        contador += thread_1_for_4.num_Primes();
-        contador += thread_2_for_4.num_Primes();
-        contador += thread_3_for_4.num_Primes();
-        contador += thread_4_for_4.num_Primes();
-        */
+        }
 
-        long time = System.currentTimeMillis() - start;
-
-
-        System.out.println("La cantidad de primos es " + contador);
-        System.out.println("Tiempo de demora es milisegundos " + time);
     }
 }
